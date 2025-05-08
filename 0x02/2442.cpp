26f62ffa42ed8a3e28fa37fcb29eb6d974c1113f
Yoo-JiWon <220246055@sungshin.ac.kr>
2025-05-09 06:12:33 +0900

    Add 2442.cpp (별찍기5 풀이)

---
+++ 0x02/2442.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        // 공백 출력: N - i개
+        for (int j = 1; j <= N - i; j++) cout << ' ';
+        // 별 출력: 2*i - 1개
+        for (int j = 1; j <= 2 * i - 1; j++) cout << '*';
+        cout << '\n';
+    }
+
+    return 0;
+}
