f07c7e26a4c9e3b18a0ab9bbaba145b66da98811
Yoo-JiWon <220246055@sungshin.ac.kr>
2025-05-16 04:29:45 +0900

    11328.cpp(애너그램 만들기 문제풀이)

---
+++ 0x03/1919.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char char1[1001], char2[1001];
+    cin >> char1 >> char2;
+    
+    int count1[26] = {0};
+    int count2[26] = {0};
+
+    for (int i = 0; char1[i] != '\0'; i++){
+        count1[char1[i]-'a']++;
+    }
+
+    for (int i = 0; char2[i] != '\0'; i++) {
+        count2[char2[i] - 'a']++;
+    }
+
+    int result = 0;
+    for (int i = 0; i < 26; i++) {
+        int diff = count1[i] - count2[i];
+        if (diff < 0) diff = -diff;
+        result += diff;
+    }
+
+    cout << result << '\n';
+    return 0;
+}
